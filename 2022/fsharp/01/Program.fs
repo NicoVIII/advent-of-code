e9bca7bf8bc53feefb638ea4f2a339f5774df151
NicoVIII <nico@nicoviii.net>
2022-12-03 20:04:15 +0000

    2022 - F# - Day 01

---
+++ 2022/fsharp/01/Program.fs
@@ -0,0 +1,37 @@
+﻿open System
+open System.IO
+
+[<Measure>]
+type cal
+
+type Elf = {
+    calories: uint<cal> list
+}
+
+// Read input file
+File.ReadLines "input.txt"
+// Transform input into a useable form
+|> Seq.fold(fun (elfes, calories) line ->
+    match line.Trim() with
+    | "" ->
+        let elf = { calories = calories }
+        (Seq.append elfes [elf], [])
+    | line ->
+        let calories_line = (UInt32.Parse line) * 1u<cal>
+        (elfes, calories_line :: calories)
+) (Seq.empty, [])
+// Finish collection of last elf
+|> fun (elfes, calories) ->
+    match calories with
+    | [] -> elfes
+    | calories -> 
+        let elf = { calories = calories }
+        Seq.append elfes [elf]
+
+// Sum up calories per elf 
+|> Seq.map (fun elf -> elf.calories |> Seq.sum)
+// Sum up top 3 calories
+|> Seq.sortDescending // For one elf you could use maxBy instead of sortDescending >> take
+|> Seq.take 3 // Change this to 1 for part 1
+|> Seq.sum
+|> printfn "Calories: %i"
---
+++ 2022/fsharp/README.md
@@ -0,0 +1,2 @@
+Place your input.txt with the input provided by Advent of Code into the day directory, e.g. `2022/fsharp/01`
+and run `dotnet run` in that folder to get the solution.
