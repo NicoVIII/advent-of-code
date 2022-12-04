c539112c1a53289cc46b80b5f0662815afeb1eff
NicoVIII <nico@nicoviii.net>
2022-12-04 09:22:49 +0000

    2022 - F# - Day 02

---
+++ 2022/fsharp/02/Program.fs
@@ -0,0 +1,110 @@
+﻿open System.IO
+
+let readInput () = File.ReadAllLines "input.txt"
+
+type Hand =
+    | Rock
+    | Paper
+    | Scissors
+
+type Round = { enemy: Hand; me: Hand }
+
+let parseInput lines =
+    let parseEnemyHand =
+        function
+        | "A" -> Rock
+        | "B" -> Paper
+        | "C" -> Scissors
+        | _ -> failwith "Invalid hand"
+
+    let parseMyHand =
+        function
+        | "X" -> Rock
+        | "Y" -> Paper
+        | "Z" -> Scissors
+        | _ -> failwith "Invalid hand"
+
+    lines
+    |> Seq.map (fun (line: string) ->
+        let parts = line.Split ' '
+
+        { enemy = parseEnemyHand parts.[0]
+          me = parseMyHand parts.[1] })
+
+let getResultScore round =
+    match round.me, round.enemy with
+    | Paper, Rock
+    | Scissors, Paper
+    | Rock, Scissors -> 6u // Win
+    | Rock, Rock
+    | Paper, Paper
+    | Scissors, Scissors -> 3u // Draw
+    | Rock, Paper
+    | Paper, Scissors
+    | Scissors, Rock -> 0u // Lose
+
+let getHandScore hand =
+    match hand with
+    | Rock -> 1u
+    | Paper -> 2u
+    | Scissors -> 3u
+
+let getScore round =
+    let resultScore = getResultScore round
+    let handScore = getHandScore round.me
+    resultScore + handScore
+
+type Result =
+    | Win
+    | Lose
+    | Draw
+
+type Round2 = { enemy: Hand; result: Result }
+
+let parseInput2 lines =
+    let parseEnemyHand =
+        function
+        | "A" -> Rock
+        | "B" -> Paper
+        | "C" -> Scissors
+        | _ -> failwith "Invalid hand"
+
+    let parseResult =
+        function
+        | "X" -> Lose
+        | "Y" -> Draw
+        | "Z" -> Win
+        | _ -> failwith "Invalid result"
+
+    lines
+    |> Seq.map (fun (line: string) ->
+        let parts = line.Split ' '
+
+        { enemy = parseEnemyHand parts.[0]
+          result = parseResult parts.[1] })
+
+let transformRound2ToRound round =
+    let myHand =
+        match round.enemy, round.result with
+        | Rock, Win
+        | Paper, Draw
+        | Scissors, Lose -> Paper
+        | Rock, Draw
+        | Paper, Lose
+        | Scissors, Win -> Rock
+        | Rock, Lose
+        | Paper, Win
+        | Scissors, Draw -> Scissors
+
+    { enemy = round.enemy; me = myHand }
+
+let input = readInput ()
+
+// Part 1
+parseInput input |> Seq.map getScore |> Seq.sum |> printfn "Score - Part 1: %i"
+
+// Part 2
+parseInput2 input
+|> Seq.map (transformRound2ToRound >> getScore)
+|> Seq.sum
+|> printfn "Score - Part 2: %i"
