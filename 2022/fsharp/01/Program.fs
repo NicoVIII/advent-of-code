306da2ae694b25c383e7b84d45535ab749af7245
NicoVIII <nico@nicoviii.net>
2022-12-04 06:45:22 +0000

    2022 - F# - Day01 - Finetune code a bit

--- 2022/fsharp/01/Program.fs
+++ 2022/fsharp/01/Program.fs
@@ -4,34 +4,41 @@ open System.IO
 [<Measure>]
 type cal
 
-type Elf = {
-    calories: uint<cal> list
-}
+type Elf = { calories: uint<cal> list }
 
-// Read input file
-File.ReadLines "input.txt"
-// Transform input into a useable form
-|> Seq.fold(fun (elfes, calories) line ->
-    match line.Trim() with
-    | "" ->
-        let elf = { calories = calories }
-        (Seq.append elfes [elf], [])
-    | line ->
-        let calories_line = (UInt32.Parse line) * 1u<cal>
-        (elfes, calories_line :: calories)
-) (Seq.empty, [])
-// Finish collection of last elf
-|> fun (elfes, calories) ->
-    match calories with
-    | [] -> elfes
-    | calories -> 
-        let elf = { calories = calories }
-        Seq.append elfes [elf]
+let readInput () =
+    // Read input file
+    File.ReadLines "input.txt"
+    // Transform input into a useable form
+    |> Seq.fold
+        (fun (elfes, calories) line ->
+            match line.Trim() with
+            | "" ->
+                let elf = { calories = calories }
+                (Seq.append elfes [ elf ], [])
+            | line ->
+                let calories_line = (UInt32.Parse line) * 1u<cal>
+                (elfes, calories_line :: calories))
+        (Seq.empty, [])
+    // Finish collection of last elf
+    |> fun (elfes, calories) ->
+        match calories with
+        | [] -> elfes
+        | calories ->
+            let elf = { calories = calories }
+            Seq.append elfes [ elf ]
 
-// Sum up calories per elf 
-|> Seq.map (fun elf -> elf.calories |> Seq.sum)
-// Sum up top 3 calories
-|> Seq.sortDescending // For one elf you could use maxBy instead of sortDescending >> take
-|> Seq.take 3 // Change this to 1 for part 1
-|> Seq.sum
-|> printfn "Calories: %i"
+let inline sum_biggest x calories =
+    match x with
+    | 1 -> Seq.max calories
+    | x when x > 1 -> calories |> Seq.sortDescending |> Seq.take x |> Seq.sum
+    | _ -> failwith "x must be greater than 0"
+
+// Prepare input
+let calories = readInput () |> Seq.map (fun elf -> Seq.sum elf.calories) // Sum all calories per elf
+
+// Part 1
+sum_biggest 1 calories |> printfn "Calories - Part 1: %i"
+
+// Part 2
+sum_biggest 3 calories |> printfn "Calories - Part 2: %i"
